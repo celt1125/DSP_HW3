1311d8f2f19a55904ec3585082aaa208372677d4
Andy Hsu <andyhsu@livapp.com>
2022-12-02 09:30:26 +0800

    Fix wrong unicode

--- prep.sh
+++ prep.sh
@@ -7,5 +7,5 @@ done
 
 # setup language model
 perl ./separator_big5.pl ./corpus.txt > ./corpus_sep.txt
-ngram-count ‒text ./corpus_sep.txt ‒write ./ngram_cnt.txt ‒order 2
-ngram-count ‒read ./ngram_cnt.txt ‒lm ./ngram_lm.txt ‒order 2 -unk
+ngram-count -text ./corpus_sep.txt -write ./ngram.count -order 2
+ngram-count -read ./ngram_cnt.txt -lm ./ngram_lm.txt -order 2 -unk
